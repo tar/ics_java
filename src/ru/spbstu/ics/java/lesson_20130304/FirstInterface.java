8bceca6f00d5e49205a697b79a2713f9731ee7d1
Anton Lukashin <an.lukashin@gmail.com>
2013-03-17 21:54:18 +0400

    clear master BraNCH

--- src/ru/spbstu/ics/java/lesson_20130304/FirstInterface.java
+++
@@ -1,7 +0,0 @@
-package ru.spbstu.ics.java.lesson_20130304;
-
-public interface FirstInterface {
-	
-	public final int integer=1;
-	public String getString();
-}
--- src/ru/spbstu/ics/java/lesson_20130304/UseEnum.java
+++
@@ -1,9 +0,0 @@
-package ru.spbstu.ics.java.lesson_20130304;
-
-public class UseEnum {
-	public static void main(String[] args) {
-		for (int i = 0; i < FirstEnum.values().length; i++) {
-			System.out.println(FirstEnum.values()[i]);
-		}
-	}
-}
